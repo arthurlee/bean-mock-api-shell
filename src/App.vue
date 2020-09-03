1dfc9a0e8b160faad069e9a5c6eed9433ebc48cd
arthurlee <boyal2008@gmail.com>
2020-09-03 23:30:04 +0800

    remove HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,17 @@
       </p>
       <el-button>el-button</el-button>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
